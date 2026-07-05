7d1e7cbad56ecc7ab59579b69d39f153974090ad
seagin <cijin@seagin.me>
2026-07-05 15:17:51 +0700

    copied buffer creation from example

--- src/main.c
+++ src/main.c
@@ -54,24 +54,58 @@ int allocate_shm_file(size_t size) {
   return fd;
 }
 
-void draw_frame() {
+static void wl_buffer_release(void *data, struct wl_buffer *wl_buffer)
+{
+    /* Sent by the compositor when it's no longer using this buffer */
+    wl_buffer_destroy(wl_buffer);
+}
+
+static const struct wl_buffer_listener wl_buffer_listener = {
+    .release = wl_buffer_release,
+};
+
+static struct wl_buffer *draw_frame(struct client_state *state) {
   const int height = 720, width = 480;
   const int stride = width * 4;
-  const shm_pool_size = height * stride * 2;
+  const int shm_pool_size = height * stride * 2;
 
   int fd = allocate_shm_file(shm_pool_size);
   assert(fd > 0 && "shm allocation failed");
 
-  // Todo: mmap memory just allocated
+  uint32_t *data = mmap(NULL, shm_pool_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+  if (data == MAP_FAILED) {
+    close(fd);
+    return NULL;
+  }
+
+  struct wl_shm_pool *pool = wl_shm_create_pool(state->wl_shm, fd, shm_pool_size);
+  struct wl_buffer *buffer = wl_shm_pool_create_buffer(pool, 0,
+      width, height, stride, WL_SHM_FORMAT_XRGB8888);
+  wl_shm_pool_destroy(pool);
+  close(fd);
+
+  /* Draw checkerboxed background */
+  for (int y = 0; y < height; ++y) {
+    for (int x = 0; x < width; ++x) {
+      if ((x + y / 8 * 8) % 16 < 8)
+        data[y * width + x] = 0xFF666666;
+      else
+        data[y * width + x] = 0xFFEEEEEE;
+    }
+  }
+
+  munmap(data, shm_pool_size);
+  wl_buffer_add_listener(buffer, &wl_buffer_listener, NULL);
+  return buffer;
 }
 
 void xdg_surface_configure(void *data, struct xdg_surface *xdg_surface, uint32_t serial) {
   struct client_state *state = data;
   xdg_surface_ack_configure(xdg_surface, serial);
-  // Todo:
-  // Draw frame
-  // Attach surface
-  // Commit surface
+
+  struct wl_buffer *buffer = draw_frame(state);
+  wl_surface_attach(state->wl_surface, buffer, 0, 0);
+  wl_surface_commit(state->wl_surface);
 }
 
 struct xdg_surface_listener xdg_surface_listener = {
@@ -144,11 +178,12 @@ int main(int argc, char *argv[]) {
   assert(xdg_surface_listener_status != -1);
 
   state.xdg_toplevel = xdg_surface_get_toplevel(state.xdg_surface);
+  xdg_toplevel_set_title(state.xdg_toplevel, "Zoomer Client");
+  wl_surface_commit(state.wl_surface);
 
-  // Todo:
-  // Create toplevel
-  // Create shm
-  // Let wayland know about shm
+  while (wl_display_dispatch(state.wl_display)) {
+    /* This space deliberately left blank */
+  }
 
   wl_display_disconnect(state.wl_display);
   return 0;
